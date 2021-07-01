c002231cc743972aea004461c4e62feb2c61f9a6
Sanya Oluwadare <sanyaodare@gmail.com>
2021-07-01 20:14:24 +0100

    update scripts

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 let ball = document.getElementById('float-circle');
 
-// Write your code below
+// function named up() that will raise the bottom
+function up() {
+  ball.style.bottom = '250px';
+};
 
+//function named down() that will lower the bottom position of the ball
+function down() {
+ball.style.bottom = '50px';
+}
+document.body.addEventListener("keydown", up);
+document.body.addEventListener("keyup", down);
